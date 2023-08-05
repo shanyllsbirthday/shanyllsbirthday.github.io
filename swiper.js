263f0b0e0a93fd608dbff84a09c27c9810ec7fbc
shanyllsbirthday <141213737+shanyllsbirthday@users.noreply.github.com>
2023-08-05 10:08:48 +0800

    added swiperjs

---
+++ swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 1,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
