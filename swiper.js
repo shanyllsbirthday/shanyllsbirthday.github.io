4265cab746ab36bad46836373632b6f163229b48
shanyllsbirthday <141213737+shanyllsbirthday@users.noreply.github.com>
2023-08-05 14:01:39 +0800

    ADDED SWIPER RIGHT

--- swiper.js
+++ swiper.js
@@ -1,16 +1,34 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 1,
-    centeredSlides: true,
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 1,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper1 = new Swiper(".mySwiperTwo", {
+  spaceBetween: 1,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
