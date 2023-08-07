f81fabc4c34ea6db32f270725b72886fc48661bb
shanyllsbirthday <141213737+shanyllsbirthday@users.noreply.github.com>
2023-08-07 14:33:26 +0800

    added happiness audio

--- secret.js
+++ secret.js
@@ -1,36 +1,35 @@
- // Get the modal
- var modal = document.getElementById("passwordModal");
- var secretBtn = document.getElementById("wrong");
+// Get the modal
+var modal = document.getElementById("passwordModal");
+var secretBtn = document.getElementById("wrong");
+var audioTwo = document.getElementById('myAudioTwo');
 
-secretBtn.addEventListener('click', function(){
-    modal.style.display = 'none';
+secretBtn.addEventListener('click', function () {
+  modal.style.display = 'none';
 });
 
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function (event) {
-   if (event.target === modal) {
-     modal.style.display = "none";
-   }
- };
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
 
+function checkPassword() {
+  var enteredPassword = document.getElementById("passwordInput").value;
+  var correctPassword = "2/11/2023";
 
+  if (enteredPassword === correctPassword) {
+    audioTwo.play();
+    modal.style.display = "none";
+    document.getElementById("errorMessage").style.display = "none";
+    document.querySelector(".container").style.display = "none";
+    document.getElementById("withPalanggaBestMoments").style.display = "block";
+  } else {
+    document.getElementById("errorMessage").style.display = "block";
+  }
+}
 
-
- function checkPassword() {
-   var enteredPassword = document.getElementById("passwordInput").value;
-   var correctPassword = "2/11/2023";
-
-   if (enteredPassword === correctPassword) {
-     modal.style.display = "none";
-     document.getElementById("errorMessage").style.display = "none";
-     document.querySelector(".container").style.display = "none";
-     document.getElementById("withPalanggaBestMoments").style.display = "block";
-   } else {
-     document.getElementById("errorMessage").style.display = "block";
-   }
- }
-
- //show model when click button
- document.getElementById("secret").onclick = function () {
-   modal.style.display = "block";
- };
+//show model when click button
+document.getElementById("secret").onclick = function () {
+  modal.style.display = "block";
+};
